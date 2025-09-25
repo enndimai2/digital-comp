5f25c948b41925a1bab6609347621fee00bf4917
jinxuan <kellyleejinxuanlee@gmail.com>
2025-09-25 12:26:25 +0800

    English Sci-Fi + Thriller movies added!
    <script src="../movies-english-scifi-thriller.js"></script>
      <script>
        // Load movies from all included JS files
        let movies = [];
        if (typeof defineMovies === 'function') {
          movies = defineMovies();
        }

---
+++ movies-english-scifi-thriller.js
@@ -0,0 +1,318 @@
+// English Sci-Fi + Thriller movies
+defineMovies = () => [
+  // ==== Sci-Fi ====
+  {
+    title: "Inception",
+    year: 2010,
+    genre: ["Sci-Fi", "Thriller"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/51xJxV6fJ6L._AC_.jpg",
+    imdb: "https://www.imdb.com/title/tt1375666/"
+  },
+  {
+    title: "Interstellar",
+    year: 2014,
+    genre: ["Sci-Fi", "Drama"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71n58VLG3nL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0816692/"
+  },
+  {
+    title: "The Matrix",
+    year: 1999,
+    genre: ["Sci-Fi", "Action"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/51EG732BV3L.jpg",
+    imdb: "https://www.imdb.com/title/tt0133093/"
+  },
+  {
+    title: "The Matrix Reloaded",
+    year: 2003,
+    genre: ["Sci-Fi", "Action"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/51VPFqxu7-L._AC_.jpg",
+    imdb: "https://www.imdb.com/title/tt0234215/"
+  },
+  {
+    title: "The Matrix Revolutions",
+    year: 2003,
+    genre: ["Sci-Fi", "Action"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/51AdNBW6AAL._AC_.jpg",
+    imdb: "https://www.imdb.com/title/tt0242653/"
+  },
+  {
+    title: "The Matrix Resurrections",
+    year: 2021,
+    genre: ["Sci-Fi", "Action"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71nMDEt2x-L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt10838180/"
+  },
+  {
+    title: "Blade Runner",
+    year: 1982,
+    genre: ["Sci-Fi", "Drama"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71KWfY3I4dL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0083658/"
+  },
+  {
+    title: "Blade Runner 2049",
+    year: 2017,
+    genre: ["Sci-Fi", "Thriller"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81oWw0ZmLqL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1856101/"
+  },
+  {
+    title: "Arrival",
+    year: 2016,
+    genre: ["Sci-Fi", "Drama"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71tzmrB9j3L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt2543164/"
+  },
+  {
+    title: "Gravity",
+    year: 2013,
+    genre: ["Sci-Fi", "Thriller"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91b4rlIol7L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1454468/"
+  },
+  {
+    title: "The Martian",
+    year: 2015,
+    genre: ["Sci-Fi", "Adventure"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91kFYg4fX3L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt3659388/"
+  },
+  {
+    title: "Ex Machina",
+    year: 2014,
+    genre: ["Sci-Fi", "Thriller"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71tqI43LZUL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0470752/"
+  },
+  {
+    title: "District 9",
+    year: 2009,
+    genre: ["Sci-Fi", "Thriller"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71A9qPAahtL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1136608/"
+  },
+  {
+    title: "Minority Report",
+    year: 2002,
+    genre: ["Sci-Fi", "Action"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/61QhOkTJ8ML._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0181689/"
+  },
+  {
+    title: "Edge of Tomorrow",
+    year: 2014,
+    genre: ["Sci-Fi", "Action"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81bRGM3P7yL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1631867/"
+  },
+  {
+    title: "Oblivion",
+    year: 2013,
+    genre: ["Sci-Fi", "Adventure"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71Xz3d8RauL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1483013/"
+  },
+  {
+    title: "I Am Legend",
+    year: 2007,
+    genre: ["Sci-Fi", "Thriller"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71X4Mryi3gL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0480249/"
+  },
+  {
+    title: "War of the Worlds",
+    year: 2005,
+    genre: ["Sci-Fi", "Thriller"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/51gNqEYPWYLS._AC_.jpg",
+    imdb: "https://www.imdb.com/title/tt0407304/"
+  },
+  {
+    title: "Ready Player One",
+    year: 2018,
+    genre: ["Sci-Fi", "Adventure"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91WZyh+zVYL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1677720/"
+  },
+  {
+    title: "Elysium",
+    year: 2013,
+    genre: ["Sci-Fi", "Action"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91E8R8Zr1zL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1535108/"
+  },
+
+  // ==== Thriller ====
+  {
+    title: "Se7en",
+    year: 1995,
+    genre: ["Thriller", "Crime"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71lVwlzWHhL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0114369/"
+  },
+  {
+    title: "Fight Club",
+    year: 1999,
+    genre: ["Thriller", "Drama"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81D+KJkOQ-L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0137523/"
+  },
+  {
+    title: "The Silence of the Lambs",
+    year: 1991,
+    genre: ["Thriller", "Crime"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81fR2zYY6RL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0102926/"
+  },
+  {
+    title: "Gone Girl",
+    year: 2014,
+    genre: ["Thriller", "Mystery"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81B9n6YJpDL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt2267998/"
+  },
+  {
+    title: "Shutter Island",
+    year: 2010,
+    genre: ["Thriller", "Mystery"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91XajKnfBXL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1130884/"
+  },
+  {
+    title: "Prisoners",
+    year: 2013,
+    genre: ["Thriller", "Crime"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91KNHeF4RJL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1392214/"
+  },
+  {
+    title: "Zodiac",
+    year: 2007,
+    genre: ["Thriller", "Mystery"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81chmFFfX9L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0443706/"
+  },
+  {
+    title: "Memento",
+    year: 2000,
+    genre: ["Thriller", "Mystery"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81hWgk8iThL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0209144/"
+  },
+  {
+    title: "The Prestige",
+    year: 2006,
+    genre: ["Thriller", "Drama"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81y+0yL7twL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0482571/"
+  },
+  {
+    title: "No Country for Old Men",
+    year: 2007,
+    genre: ["Thriller", "Crime"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81NK1wxPmoL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0477348/"
+  },
+  {
+    title: "American Psycho",
+    year: 2000,
+    genre: ["Thriller", "Drama"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81VRtWYtD4L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0144084/"
+  },
+  {
+    title: "The Girl with the Dragon Tattoo",
+    year: 2011,
+    genre: ["Thriller", "Mystery"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81k22zZ1BJL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1568346/"
+  },
+  {
+    title: "Nightcrawler",
+    year: 2014,
+    genre: ["Thriller", "Crime"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91p2L2uO03L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt2872718/"
+  },
+  {
+    title: "Oldboy (US Remake)",
+    year: 2013,
+    genre: ["Thriller", "Drama"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/71K3b04oZ9L._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt1321511/"
+  },
+  {
+    title: "The Sixth Sense",
+    year: 1999,
+    genre: ["Thriller", "Mystery"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81+U5FA3hCL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt0167404/"
+  },
+  {
+    title: "A Quiet Place",
+    year: 2018,
+    genre: ["Thriller", "Horror"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81pLWXgFnyL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt6644200/"
+  },
+  {
+    title: "A Quiet Place Part II",
+    year: 2020,
+    genre: ["Thriller", "Horror"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91h2MvmP2nL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt8332922/"
+  },
+  {
+    title: "Tenet",
+    year: 2020,
+    genre: ["Sci-Fi", "Thriller"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/81IkkAKqvUL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt6723592/"
+  },
+  {
+    title: "Don’t Look Up",
+    year: 2021,
+    genre: ["Sci-Fi", "Comedy"],
+    language: "English",
+    poster: "https://m.media-amazon.com/images/I/91J2tPznktL._AC_SY679_.jpg",
+    imdb: "https://www.imdb.com/title/tt11286314/"
+  }
+];
---
+++ templates/# Code Citations.md
@@ -0,0 +1,62 @@
+# Code Citations
+
+## License: unknown
+https://github.com/sadafwalliyani/Web-3.O/tree/d3dcf0026647a100cdeba471fe95ab01c9417c1e/Java-Assignments/Assignment-4/index.html
+
+```
+>
+    <label for="genre">Genre:</label>
+    <select id="genre">
+      <option value="">--Select Genre--</option>
+      <option value="Action">Action</option>
+      <option
+```
+
+
+## License: unknown
+https://github.com/erezstmn/my-movie-app-client/tree/35537bae65f40e101c373d2cf32709a65dbb341f/src/App.js
+
+```
+/option>
+      <option value="Comedy">Comedy</option>
+      <option value="Drama">Drama</option>
+      <option value="Sci-Fi">Sci-Fi</option>
+      <option value="Thriller">Thriller
+```
+
+
+## License: unknown
+https://github.com/Jokatsu/StoryTime/tree/e55f3aecf56f56008281c0dd5b2632476c6c1a5a/views/story.handlebars
+
+```
+>
+      <option value="Drama">Drama</option>
+      <option value="Sci-Fi">Sci-Fi</option>
+      <option value="Thriller">Thriller</option>
+      <option value="Romance">Romance</
+```
+
+
+## License: unknown
+https://github.com/walkerfloyd1/StoryTeller/tree/36cca698029e196695d577afe5a5dc6122feba00/client/src/Front%20End%20Files%20To%20Be%20Reconfigured.../views/story.handlebars
+
+```
+</option>
+      <option value="Drama">Drama</option>
+      <option value="Sci-Fi">Sci-Fi</option>
+      <option value="Thriller">Thriller</option>
+      <option value="Romance">
+```
+
+
+## License: unknown
+https://github.com/Hallvard-Benan/assignment-html-css/tree/2094ac7c128598eb6b50ec75de8bcd0d7cdc28ec/index.html
+
+```
+</option>
+      <option value="Action">Action</option>
+      <option value="Comedy">Comedy</option>
+      <option value="Drama">Drama</option>
+      <option value="Sci-Fi">Sci-
+```
+
